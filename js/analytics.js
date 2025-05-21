57cc7758094c2a6401e0c6e6dbab665ba9822282
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-05-21 11:28:45 -0300

    vercel- added functions

--- js/analytics.js
+++ js/analytics.js
@@ -6,32 +6,30 @@ function gtag(){dataLayer.push(arguments);}
 
 
 function clicar_botao(){
-    document.getElementById('botao-teste').addEventListener('click', function() {
-        gtag('event', 'clique_botao', {
-        'event_category': 'interação',
-        'event_label': 'botao_teste',
-        'value': 1
-        });
+
+    gtag('event', 'clique_botao', {
+    'event_category': 'interação',
+    'event_label': 'botao_teste',
+    'value': 1
     });
+    
 }
 
 
 function bem_vindo(){
-    document.getElementById('botao-bem_vindo').addEventListener('click', function() {
         gtag('event', 'bem_vindo_page', {
         'event_category': 'interação',
         'event_label': 'bem_vindo',
         'value': 1
         });
-    });
+    
 }
 
 function home_test(){
-    document.getElementById('botao-home').addEventListener('click', function() {
         gtag('event', 'home', {
         'event_category': 'interação',
         'event_label': 'home_page',
         'value': 1
         });
-    });
+    
 }
