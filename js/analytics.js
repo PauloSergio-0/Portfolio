ce0040de4c67a154d1db1dec8e84f303bc95ec8f
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-05-22 11:44:51 -0300

    feat: added new Gtag

--- js/analytics.js
+++ js/analytics.js
@@ -2,7 +2,7 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-EBPE41L69T');
+    gtag('config', 'G-KM9TG6SLYV');
 
 
 /**
@@ -21,32 +21,3 @@ function eventoAnalytics(evento, categoria_evento, rotulo_evento, valor_evento){
     'value': valor_evento
     });
 }
-
-// function clicar_botao(){
-
-//     gtag('event', 'clique_botao', {
-//     'event_category': 'interação',
-//     'event_label': 'botao_teste',
-//     'value': 1
-//     });
-    
-// }
-
-
-// function bem_vindo(){
-//         gtag('event', 'bem_vindo_page', {
-//         'event_category': 'interação',
-//         'event_label': 'bem_vindo',
-//         'value': 1
-//         });
-    
-// }
-
-// function home_test(){
-//         gtag('event', 'home', {
-//         'event_category': 'interação',
-//         'event_label': 'home_page',
-//         'value': 1
-//         });
-    
-// }
