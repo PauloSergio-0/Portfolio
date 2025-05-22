71598912ab0317ff71909d6ac7bd663bc168663c
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-05-22 08:45:15 -0300

    feat: simplifying functions

--- js/analytics.js
+++ js/analytics.js
@@ -5,31 +5,48 @@ function gtag(){dataLayer.push(arguments);}
     gtag('config', 'G-EBPE41L69T');
 
 
-function clicar_botao(){
+/**
+ * Envia evento para o Google Analytics
+ * @param {String} evento - Nome do evento 
+ * @param {String} categoria_evento - Tipo de categoria
+ * @param {String} rotulo_evento - Rotulo do evento que será enviado
+ * @param {number} valor_evento - Valor do evento 
+ */
 
-    gtag('event', 'clique_botao', {
-    'event_category': 'interação',
-    'event_label': 'botao_teste',
-    'value': 1
+function eventoAnalytics(evento, categoria_evento, rotulo_evento, valor_evento){
+
+    gtag('event', evento, {
+    'event_category': categoria_evento,
+    'event_label': rotulo_evento,
+    'value': valor_evento
     });
-    
 }
 
+// function clicar_botao(){
 
-function bem_vindo(){
-        gtag('event', 'bem_vindo_page', {
-        'event_category': 'interação',
-        'event_label': 'bem_vindo',
-        'value': 1
-        });
+//     gtag('event', 'clique_botao', {
+//     'event_category': 'interação',
+//     'event_label': 'botao_teste',
+//     'value': 1
+//     });
     
-}
+// }
 
-function home_test(){
-        gtag('event', 'home', {
-        'event_category': 'interação',
-        'event_label': 'home_page',
-        'value': 1
-        });
+
+// function bem_vindo(){
+//         gtag('event', 'bem_vindo_page', {
+//         'event_category': 'interação',
+//         'event_label': 'bem_vindo',
+//         'value': 1
+//         });
     
-}
+// }
+
+// function home_test(){
+//         gtag('event', 'home', {
+//         'event_category': 'interação',
+//         'event_label': 'home_page',
+//         'value': 1
+//         });
+    
+// }
